3733858c83b3d9e05ced8499dc88d274d87b4183
Daniel Péder <daniel.peder@gmail.com>
2016-05-24 00:20:42 +0200

    version 1.0.5

--- league.meta.user.js
+++ league.meta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         slitherio-league
 // @namespace    http://slither.io/
-// @version      1.0.4
+// @version      1.0.5
 // @description  slitherio-league meta
 // @author       Daniel Peder & David Bender
 // @match        http://slither.io/
