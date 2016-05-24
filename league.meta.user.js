c36dd31b0a34e2443818901b148a619631954000
Daniel Péder <daniel.peder@gmail.com>
2016-05-25 00:15:22 +0200

    minor version version increase

--- league.meta.user.js
+++ league.meta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         slitherio-league
 // @namespace    http://slither.io/
-// @version      1.0.38
+// @version      1.0.39
 // @description  slitherio-league meta
 // @author       Daniel Peder & David Bender
 // @match        http://slither.io/
