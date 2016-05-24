a9cfda448ba40c005753fb0cdf38260c61f44edd
Daniel Péder <daniel.peder@gmail.com>
2016-05-24 17:30:51 +0200

    backend app.zone.cz

--- plugBackend.js
+++ plugBackend.js
@@ -4,25 +4,38 @@ Backend = {
     },
     PostRaw: function (raw) {
         console.log('post raw go');
-        try {
-            var response = GM_xmlhttpRequest({
-                method: "POST",
-                url: Backend._.url,
-                data: raw,
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                onload: function (response) {
-                    console.log('post raw done');
-                    console.log(response);
-                }
-            });
-            console.log('post response');
-            console.log(response);
-        } catch (e) {
-            console.log('xhr err ' + e);
+        if (GM_xmlhttpRequest) {
+            try {
+                var response = GM_xmlhttpRequest({
+                    method: "POST",
+                    url: Backend._.url,
+                    data: raw,
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    onload: function (response) {
+                        console.log('post raw done');
+                        console.log(response);
+                    }
+                });
+                console.log('post response');
+                console.log(response);
+            } catch (e) {
+                console.log('GM_xmlhttpRequest err ' + e);
+            }
         }
-    },
+        else
+        if (XMLHttpRequest) {
+            try {
+                xmlhttp = new XMLHttpRequest();
+                xmlhttp.open('POST', Backend._.url, true);
+                xmlhttp.send('hello there');
+            } catch (e) {
+                console.log('XMLHttpRequest err ' + e);
+            }
+        }
+    }
+    ,
     Run: function () {
         this.Init();
         this.Loop();
