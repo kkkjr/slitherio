5d4dd76495169c01e34e924d44e7207fa80da5c7
Daniel Péder <daniel.peder@gmail.com>
2016-05-24 14:33:46 +0200

    minor version version increase

---
+++ Slitherio.js
@@ -0,0 +1,17 @@
+Slitherio = {
+    getSnakes: function () {
+        return window.snakes;
+    },
+    getSnake: function () {
+        return window.snake;
+    },
+    getFoods: function () {
+        return window.foods;
+    },
+    isPlaying: function () {
+        return window.playing;
+    },
+    clickPlayButton: function(){
+        window.play_btn.elem.click();
+    }
+};
--- league.meta.user.js
+++ league.meta.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         slitherio-league
 // @namespace    http://slither.io/
-// @version      1.0.23
+// @version      1.0.24
 // @description  slitherio-league meta
 // @author       Daniel Peder & David Bender
 // @match        http://slither.io/
-// @require		   https://github.com/Daniel-Peder/slitherio_league/raw/master/slitherio.js
+// @require		   https://github.com/Daniel-Peder/slitherio_league/raw/master/Slitherio.js
+// @require		   https://github.com/Daniel-Peder/slitherio_league/raw/master/Backend.js
 // @require		   https://github.com/Daniel-Peder/slitherio_league/raw/master/league.user.js
 // @grant        none
 // ==/UserScript==
