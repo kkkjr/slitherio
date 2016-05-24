727437c7f595a6d54f6999f3805192510e268359
Daniel Péder <daniel.peder@gmail.com>
2016-05-24 16:26:34 +0200

    backend and cleanup

--- league.meta.user.js
+++ league.meta.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         slitherio-league
 // @namespace    http://slither.io/
-// @version      1.0.24
+// @version      1.0.25
 // @description  slitherio-league meta
 // @author       Daniel Peder & David Bender
 // @match        http://slither.io/
-// @require		   https://github.com/Daniel-Peder/slitherio_league/raw/master/Slitherio.js
-// @require		   https://github.com/Daniel-Peder/slitherio_league/raw/master/Backend.js
+// @require		   https://github.com/Daniel-Peder/slitherio_league/raw/master/plugSlitherio.js
+// @require		   https://github.com/Daniel-Peder/slitherio_league/raw/master/plugBackend.js
 // @require		   https://github.com/Daniel-Peder/slitherio_league/raw/master/league.user.js
 // @grant        none
 // ==/UserScript==
