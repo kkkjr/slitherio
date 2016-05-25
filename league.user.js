b6c17de88cdb385bef44392c02b54e9a402a475b
Daniel Péder <daniel.peder@gmail.com>
2016-05-25 12:32:45 +0200

    minor version increase

--- league.user.js
+++ league.user.js
@@ -74,7 +74,7 @@ SLApp = {
             SLApp.log('Refresh issued');
             window.setTimeout(function () {
                 SLApp.log('Refreshing now ...');
-                location.reload(1);
+                location.reload(true); // force full reload without cache
             }, 1000);
         } else {
             this.log('Plays to refresh #' + this.RefreshCheck_);
